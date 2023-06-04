bb667776bc4eb3fe6c1941fab83c6c1cf77ea0fe
Gurveer Madurai <maduraigurveer@gmail.com>
2023-06-04 11:00:30 -0700

    'through' error resolves in models/index.js

--- models/index.js
+++ models/index.js
@@ -15,11 +15,13 @@ Category.hasMany(Product,{
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
+  through: ProductTag,
   foreignKey:'product_tag_id',
   onDelete:'CASCADE',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
+  through: ProductTag,
   foreignKey:'product_tag_id',
   onDelete:'CASCADE',
 });
